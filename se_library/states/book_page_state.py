f7670f2552f348d2a008d8343f92b44f354d466d
Copter <motto5465@gmail.com>
2025-03-01 18:33:51 +0700

    feat: book page transaction

--- se_library/states/book_page_state.py
+++ se_library/states/book_page_state.py
@@ -1,11 +1,15 @@
 import reflex as rx
-from se_library.models import AvailabilityEnum, Book, BookInventory, ConditionEnum
+from se_library.models import AvailabilityEnum, Book, BookInventory, ConditionEnum, User, BorrowStatusEnum, BookTransaction
 from typing import List
+from sqlalchemy.orm import Session
 import asyncio
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import os
 from dotenv import load_dotenv
+from se_library.states.base import BaseState
+from datetime import datetime, timedelta
+from jinja2 import Environment, FileSystemLoader
 
 load_dotenv()
 
@@ -129,21 +133,155 @@ class BorrowDialogState(BookPageState):
         yield rx.toast.success(result.message, position="top-center")
         self.is_open = False
 
+    def condition_to_enum(self, condition: str) -> ConditionEnum:
+        match condition:
+            case "Factory New":
+                return ConditionEnum.FACTORY_NEW
+            case "Minimal Wear":
+                return ConditionEnum.MINIMAL_WEAR
+            case "Field Tested":
+                return ConditionEnum.FIELD_TESTED
+            case "Well Worn":
+                return ConditionEnum.WELL_WORN
+            case "Battle Scarred":
+                return ConditionEnum.BATTLE_SCARRED
+            case _:
+                return None
+            
+    def enum_to_condition(self, condition: ConditionEnum) -> str:
+        match condition:
+            case ConditionEnum.FACTORY_NEW:
+                return "Factory New"
+            case ConditionEnum.MINIMAL_WEAR:
+                return "Minimal Wear"
+            case ConditionEnum.FIELD_TESTED:
+                return "Field Tested"
+            case ConditionEnum.WELL_WORN:
+                return "Well Worn"
+            case ConditionEnum.BATTLE_SCARRED:
+                return "Battle Scarred"
+            case _:
+                return None
+
     async def make_transaction(self, condition: str) -> result:
         if condition not in self.available_condition:
             return result(error=True, message="Please select a valid condition")
         try:
-            message = Mail(
-                from_email='noreply@sethreads.life', # Using this domain before mo buying us new domain
-                to_emails='[YOUR_EMAIL_FOR_TEST]',
-                subject='Sending with Twilio SendGrid is Fun',
-                html_content='<strong>TEST TEST</strong>')
-            sg = SendGridAPIClient(api_key=SENDGRID_API_KEY)
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
+            with rx.session() as session:
+                base_state = await self.get_state(BaseState)
+                user = base_state.user
+                res = await self.database_execute(db=session, condition=condition, user=user)
+                if res.error:
+                    raise Exception(res.message)
+                res = await self.send_email(db=session, user=user)
+                if res.error:
+                    raise Exception(res.message)
+                return result(error=False, message="Transaction successful")
+        except Exception as e:
+            return result(error=True, message=f"Error: {e}")
+        
+    async def database_execute(self, db: Session, condition: str, user: User):
+        try:
+            book_details = db.exec(
+                Book.select().where(Book.isbn13 == self.isbn13)
+            ).first()
+            book_to_borrow = db.exec(
+                BookInventory.select().where(
+                    BookInventory.book_id == book_details.id,
+                    BookInventory.availability == AvailabilityEnum.AVAILABLE,
+                    BookInventory.condition == self.condition_to_enum(condition)
+                )
+            ).first()
+            if not book_to_borrow:
+                return result(error=True, message="Book not available")
+            book_to_borrow.availability = AvailabilityEnum.UNAVAILABLE
+            transaction = BookTransaction(
+                borrower_id=user.id,
+                book_inventory_id=book_to_borrow.id,
+                borrow_status=BorrowStatusEnum.PENDING,
+                duration=7,
+                borrow_date=datetime.now(),
+                return_date=datetime.now() + timedelta(days=7)
+            )
+            db.add(transaction)
+            db.commit()
             return result(error=False, message="Transaction successful")
         except Exception as e:
-            print(e)
-            return result(error=True, message=f"Error: {e}")
+            return result(error=True, message=f"{e}")
+        
+    async def send_email(self, db: Session, user: User):
+        try:
+            transaction = db.exec(
+                BookTransaction.select().where(
+                    BookTransaction.borrower_id == user.id,
+                    BookTransaction.borrow_status == BorrowStatusEnum.PENDING
+                ).order_by(BookTransaction.id.desc())
+            ).first()
+            book_to_borrow = db.exec(
+                BookInventory.select().where(BookInventory.id == transaction.book_inventory_id)
+            ).first()
+            book_details = db.exec(
+                Book.select().where(Book.id == book_to_borrow.book_id)
+            ).first()
+            owner = db.exec(
+                User.select().where(User.id == book_to_borrow.owner_id)
+            ).first()
+
+            lender_template_data = {
+                "company_name": "SE Library",
+                "lender_name": owner.username,
+                "borrow_request": True,
+                "approval_success": False,
+                "reject": False,
+                "request_id": transaction.id,
+                "borrower_name": user.username,
+                "book_title": book_details.title,
+                "book_condition": self.enum_to_condition(book_to_borrow.condition),
+                "submission_date": transaction.borrow_date,
+                "status": "Pending",
+                "action_url": "http://localhost:3000/profile/transactions"
+            }
+            borrower_template_data = {
+                "company_name": "SE Library",
+                "borrower_name": user.username,
+                "borrow_request": True,
+                "request_status": False,
+                "picked_up": False,
+                "approved": False,
+                "request_id": transaction.id,
+                "lender_name": owner.username,
+                "book_title": book_details.title,
+                "book_condition": self.enum_to_condition(book_to_borrow.condition),
+                "submission_date": transaction.borrow_date,
+                "status": "Pending",
+            }
+
+            current_dir = os.path.dirname(os.path.join(os.path.abspath(__file__)))
+            templates_dir = os.path.abspath(os.path.join(current_dir, '..', '..', "assets", "html"))
+            
+            env = Environment(loader=FileSystemLoader(templates_dir))
+            lender_template = env.get_template("lender_template.html")
+            borrower_template = env.get_template("borrower_template.html")
+
+            lender_html = lender_template.render(**lender_template_data)
+            borrower_html = borrower_template.render(**borrower_template_data)
+
+            lender_mail = Mail(
+                from_email="noreply@se-library.org",
+                to_emails=owner.email,
+                subject="Borrow Request",
+                html_content=lender_html
+            )
+            borrower_mail = Mail(
+                from_email="noreply@se-library.org",
+                to_emails=user.email,
+                subject="Borrow Request",
+                html_content=borrower_html
+            )
+
+            sg = SendGridAPIClient(api_key=SENDGRID_API_KEY)
+            response = sg.send(lender_mail)
+            response = sg.send(borrower_mail)
+            return result(error=False, message="Email sent")
+        except Exception as e:
+            return result(error=True, message=f"{e}")
